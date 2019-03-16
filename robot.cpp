1de38e74a241b887411094945b13225f2f11be06
Tyler Siwy <tyler.siwy@outlook.com>
2019-03-16 13:29:02 -0600

    pushing for ryan to continue working

--- robot.cpp
+++ robot.cpp
@@ -16,7 +16,7 @@ int Window_Height = 600;
 float hScale = 2.0;
 float eyeX = 0;
 float eyeY = 0;
-float eyeZ = 5;
+float eyeZ = -5;
 float lookAtX = 0;
 float lookAtY = 0;
 float lookAtZ = 0;
@@ -39,7 +39,8 @@ void specialKeys( int key, int x, int y );
 void rotateObjects();
 void drawEyes();
 void drawAntenna();
-void drawCube(float cubeScale, float xTrans, float yTrans, float zTrans);
+void drawCube(float xScale, float yScale, float zScale,
+	      float xTrans, float yTrans, float zTrans);
 
 int main (int argc, char **argv){
    glutInit(&argc, argv);
@@ -76,10 +77,7 @@ void Display(void){
    // Clear the color and depth
    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
    glMatrixMode(GL_MODELVIEW);
-   //drawHead();
-   //drawEyes();
-   //drawAntenna();
-   drawCube(3,0,0,0);
+   drawHead();
    glutSwapBuffers();
 }
 
@@ -117,9 +115,9 @@ void myMouse(int button, int state, int x, int y){
    switch(button){
       case GLUT_LEFT_BUTTON:
 	 if(state == GLUT_DOWN ){
-	    eyeX = -5.0;
-	    eyeY = -10.0;
-	    eyeZ = 5.0;
+	    eyeX = 0;
+	    eyeY = 0;
+	    eyeZ = -15.0;
 	 }else{
 	    eyeX = eyeY = 0;
 	    eyeZ = 5;
@@ -127,7 +125,7 @@ void myMouse(int button, int state, int x, int y){
 	 break;
       case GLUT_RIGHT_BUTTON:
 	 if(state == GLUT_DOWN ){
-	    eyeX = eyeY = eyeZ = 5.0;
+	    eyeX = eyeY = eyeZ = -5.0;
 	 }else{
 	    eyeX = eyeY =0;
 	    eyeZ = 5;
@@ -139,16 +137,18 @@ void drawEyes(){
    float eyeRadius = 0.5;
    float eyeHeight = 0.25;
    float resolution = 10;
-   float headCenterOffset = hScale * 2 + 0.01;
-   //Right Eye
-   glPushMatrix();
-   glTranslatef(hScale/2, hScale/3, -headCenterOffset);
+   float headCenterOffset = hScale + 0.01;
+   //Right EyebackHeadScale
+   glPushMatrix();   
+   glTranslatef(hScale/2, hScale/3, headCenterOffset);
+   glRotatef(180, 0, 1, 0);
    glColor3f(1.0,1.0, 1.0);
    gluDisk(eyeQuad, 0, eyeRadius, resolution, 1);
    glPopMatrix();
    //Left Eye
    glPushMatrix();
-   glTranslatef(-hScale/2, hScale/3, -headCenterOffset);
+   glTranslatef(-hScale/2, hScale/3, headCenterOffset);
+      glRotatef(180, 0, 1, 0);
    glColor3f(1.0, 1.0, 1.0);
    gluDisk(eyeQuad, 0, eyeRadius, resolution, 1);
    glPopMatrix();
@@ -159,131 +159,96 @@ void drawAntenna(){
    float antHeight = 1.35;
    float antRotation = 270;
    glPushMatrix();
-   glTranslatef(0, hScale, -hScale);
+   glTranslatef(0, hScale, 0);
    glRotatef(antRotation, 1.0, 0 ,0.0);
    glColor3f(0.6,0.1,0.3); //Purple
    gluCylinder(antQuad, antRadius, 0.05, antHeight, 10, 10); //Cone Shaped
+   drawCube(0.5, 0.1, 0.2, 0, 0, antHeight);
    glPopMatrix();
    
 }
 
 void drawHead(){  
+   float backHeadScale = hScale * 0.55;
    glPushMatrix();
    glRotatef(rotate_cube, 0.0, 0.0, 1.0);
-   glTranslatef(0 ,0,-2);
-   // Puke Green - FRONT
+   drawCube(hScale, hScale, hScale, 0, 0, 0);
+   
+   drawEyes();
+   drawAntenna();
+   // square on back of head for identification
    glBegin(GL_POLYGON);
-   glColor3f(0.1, 0.5, 0.5);
-   glVertex3f(hScale, -hScale, -hScale); 
-   glVertex3f(hScale, hScale, -hScale); 
-   glVertex3f(-hScale, hScale, -hScale);
-   glVertex3f(-hScale, -hScale, -hScale); 
+   glColor3f(0.5, 0.5, 1.0);
+   glVertex3f(backHeadScale, -backHeadScale, -hScale-0.1);
+   glVertex3f(backHeadScale, backHeadScale, -hScale-0.1);
+   glVertex3f(-backHeadScale, backHeadScale,-hScale-0.1);
+   glVertex3f(-backHeadScale, -backHeadScale, -hScale-0.1);
    glEnd();
- 
-   // White side - BACK
-   glBegin(GL_POLYGON);
-   glColor3f(1.0, 1.0, 1.0);
-   glVertex3f(hScale, -hScale, hScale);
-   glVertex3f(hScale, hScale, hScale);
-   glVertex3f(-hScale, hScale, hScale);
-   glVertex3f(-0.5, -hScale, hScale);
-   glEnd();
- 
-   // Purple side - RIGHT
-   glBegin(GL_POLYGON);
-   glColor3f(1.0, 0.0, 1.0);
-   glVertex3f( hScale, -hScale, hScale);
-   glVertex3f( hScale, hScale, hScale);
-   glVertex3f( hScale, hScale, -hScale);
-   glVertex3f( hScale, -hScale, -hScale);
-   glEnd();
- 
-   // Green side - LEFT
-   glBegin(GL_POLYGON);
-   glColor3f(0.0, 1.0, 0.0);
-   glVertex3f(-hScale, -hScale, -hScale);
-   glVertex3f(-hScale, hScale, -hScale);
-   glVertex3f(-hScale, hScale, hScale);
-   glVertex3f(-hScale, -hScale, hScale);
-   glEnd();
- 
-   // Blue side - TOP
-   glBegin(GL_POLYGON);
-   glColor3f(0.0, 0.0, 1.0);
-   glVertex3f(-hScale, hScale, hScale);
-   glVertex3f(-hScale, hScale, -hScale);
-   glVertex3f(hScale, hScale, -hScale);
-   glVertex3f(hScale, hScale, hScale);
-   glEnd();
- 
-   // Red side - BOTTOM
-   glBegin(GL_POLYGON);
-   glColor3f(1.0, 0.0, 0.0);
-   glVertex3f(-hScale, -hScale, -hScale);
-   glVertex3f(-hScale, -hScale, hScale);
-   glVertex3f(hScale, -hScale, hScale);
-   glVertex3f(hScale, -hScale, -hScale);
-   glEnd();
-
    glPopMatrix();
 }
 
-void drawCube(float cubeScale, float xTrans, float yTrans, float zTrans){  
+void drawCube(float xScale, float yScale, float zScale,
+	      float xTrans, float yTrans, float zTrans){  
    glPushMatrix();
    glTranslatef(xTrans ,yTrans, zTrans);
-
+   /*Cutting these in half since each vertex is the scale distance from the
+     centerpoint and we want it to be to the scale provided, not twice as large.
+     We could probably find a better solution later. */
+   /*xScale = xScale / 2;
+   yScale = yScale / 2;
+   zScale = zScale / 2;*/
    // Puke Green - FRONT
    glBegin(GL_POLYGON);
    glColor3f(0.1, 0.5, 0.5);
-   glVertex3f(hScale, -hScale, -hScale); 
-   glVertex3f(hScale, hScale, -hScale); 
-   glVertex3f(-hScale, hScale, -hScale);
-   glVertex3f(-hScale, -hScale, -hScale); 
+   glVertex3f(xScale, -yScale, -zScale); 
+   glVertex3f(xScale, yScale, -zScale); 
+   glVertex3f(-xScale, yScale, -zScale);
+   glVertex3f(-xScale, -yScale, -zScale); 
    glEnd();
  
-   // White side - BACK
+   // Brown Face - BACK
    glBegin(GL_POLYGON);
-   glColor3f(1.0, 1.0, 1.0);
-   glVertex3f(hScale, -hScale, hScale);
-   glVertex3f(hScale, hScale, hScale);
-   glVertex3f(-hScale, hScale, hScale);
-   glVertex3f(-0.5, -hScale, hScale);
+   glColor3f(0.8, 0.5, 0.3);  
+   glVertex3f(-xScale, -yScale, zScale);
+   glVertex3f(-xScale, yScale, zScale);
+   glVertex3f(xScale, yScale, zScale);
+   glVertex3f(xScale, -yScale, zScale);
    glEnd();
  
    // Purple side - RIGHT
    glBegin(GL_POLYGON);
    glColor3f(1.0, 0.0, 1.0);
-   glVertex3f( hScale, -hScale, hScale);
-   glVertex3f( hScale, hScale, hScale);
-   glVertex3f( hScale, hScale, -hScale);
-   glVertex3f( hScale, -hScale, -hScale);
+   glVertex3f( xScale, -yScale, zScale);
+   glVertex3f( xScale, yScale, zScale);
+   glVertex3f( xScale, yScale, -zScale);
+   glVertex3f( xScale, -yScale, -zScale);
    glEnd();
  
    // Green side - LEFT
    glBegin(GL_POLYGON);
    glColor3f(0.0, 1.0, 0.0);
-   glVertex3f(-hScale, -hScale, -hScale);
-   glVertex3f(-hScale, hScale, -hScale);
-   glVertex3f(-hScale, hScale, hScale);
-   glVertex3f(-hScale, -hScale, hScale);
+   glVertex3f(-xScale, -yScale, -zScale);
+   glVertex3f(-xScale, yScale, -zScale);
+   glVertex3f(-xScale, yScale, zScale);
+   glVertex3f(-xScale, -yScale, zScale);
    glEnd();
  
    // Blue side - TOP
    glBegin(GL_POLYGON);
    glColor3f(0.0, 0.0, 1.0);
-   glVertex3f(-hScale, hScale, hScale);
-   glVertex3f(-hScale, hScale, -hScale);
-   glVertex3f(hScale, hScale, -hScale);
-   glVertex3f(hScale, hScale, hScale);
+   glVertex3f(-xScale, yScale, zScale);
+   glVertex3f(-xScale, yScale, -zScale);
+   glVertex3f(xScale, yScale, -zScale);
+   glVertex3f(xScale, yScale, zScale);
    glEnd();
  
    // Red side - BOTTOM
    glBegin(GL_POLYGON);
    glColor3f(1.0, 0.0, 0.0);
-   glVertex3f(-hScale, -hScale, -hScale);
-   glVertex3f(-hScale, -hScale, hScale);
-   glVertex3f(hScale, -hScale, hScale);
-   glVertex3f(hScale, -hScale, -hScale);
+   glVertex3f(-xScale, -yScale, -zScale);
+   glVertex3f(-xScale, -yScale, zScale);
+   glVertex3f(xScale, -yScale, zScale);
+   glVertex3f(xScale, -yScale, -zScale);
    glEnd();
 
    glPopMatrix();
