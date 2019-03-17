dfb7f8f05254c781d50c992fd611270cb4ab0c87
Tyler Siwy <tyler.siwy@outlook.com>
2019-03-17 11:28:20 -0600

    Added head rotation

--- robot.cpp
+++ robot.cpp
@@ -41,6 +41,7 @@ float headZ = 0;
 float neckX = 0;
 float neckY = 0;
 float neckZ = 0;
+float headRotationAngle = 0;
 
 static bool paused=false;
 
@@ -57,6 +58,7 @@ void drawAntenna();
 void drawNeck();
 void rotateAntena();
 void drawBody();
+void drawAndRotateHead();
 
 int main (int argc, char **argv){
    glutInit(&argc, argv);
@@ -85,7 +87,7 @@ int main (int argc, char **argv){
    MyInit();
 
 
-     glutMainLoop(); 
+    glutMainLoop(); 
    return 0;
 }
 
@@ -98,7 +100,7 @@ void Display(void){
    // Clear the color and depth
    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
    glMatrixMode(GL_MODELVIEW);
-   drawHead();
+   drawAndRotateHead();
    drawBody();
    glutSwapBuffers();
 }
@@ -217,28 +219,21 @@ void myKeyboardKey(unsigned char key, int x, int y){
    }
 }
 
+//For debugging, to be changed later
 void myMouse(int button, int state, int x, int y){
    switch(button){
       case GLUT_LEFT_BUTTON:
 	 if(state == GLUT_DOWN ){
-	    
-	        eyeX = 0.0;
-	        eyeY = 0.0;
-	        eyeZ = 15.0;
-		//	cout<< paused;
+	    headRotationAngle = 90;
 	 }else{
-	        eyeX = eyeY = 0;
-	        eyeZ = -15;
+	    headRotationAngle = 0;
 	 }
 	 break;
       case GLUT_RIGHT_BUTTON:
 	 if(state == GLUT_DOWN ){
-	    eyeX = 15.0;
-	    eyeY = 0.0;
-	    eyeZ = 0.0;
+	    headRotationAngle = -90;
 	 }else{
-	    eyeX = eyeY =0;
-	    eyeZ = 5;
+	    headRotationAngle = 0;
 	 }
 	 break;	 
    }
@@ -265,6 +260,26 @@ void drawHead(){
    glMatrixMode(GL_MODELVIEW);
 }
 
+void drawAndRotateHead(){
+   glRotatef(headRotationAngle, 0, 1, 0);
+   drawHead();
+   glRotatef(-headRotationAngle, 0, 1, 0);
+}
+
+void drawNeck(){
+   float neckToHeadRatio = 0.70;
+   float neckRadius = hScale * neckToHeadRatio;
+   float neckDiameter = neckRadius *2;     
+   float neckHeight = 1;
+   glPushMatrix();
+   glColor3f(1.0, 0.27, 0);
+   // I am using thaumaturgy here to find the proper location
+   glTranslatef(headX, headY-neckHeight*2, -hScale+neckDiameter*neckToHeadRatio);
+   glRotatef(90, 1, 0, 0);
+   gluCylinder(neckQuad, neckRadius, neckRadius, neckHeight, 10, 10);
+   glPopMatrix();
+}
+
 void drawEyes(){
    float eyeRadius = 0.5;
    float resolution = 10;
@@ -285,7 +300,7 @@ void drawEyes(){
    glPopMatrix();
 }
 void rotateAntena(){
-   antAngle += 10;
+   antAngle += 5 - headRotationAngle;
    glTranslatef(antX, antY, antZ);
    glRotatef(antAngle, 0, 1, 0);
    glTranslatef(-antX, -antY, -antZ);
@@ -305,20 +320,6 @@ void drawAntenna(){
    drawCube(0.8, 0.3, 0.1, 0, 0, antHeight);
    glPopMatrix();
    
-}
-
-void drawNeck(){
-   float neckToHeadRatio = 0.70;
-   float neckRadius = hScale * neckToHeadRatio;
-   float neckDiameter = neckRadius *2;     
-   float neckHeight = 1;
-   glPushMatrix();
-   glColor3f(1.0, 0.27, 0);
-   // I am using thaumaturgy here to find the proper location
-   glTranslatef(headX, headY-neckHeight*2, -hScale+neckDiameter*neckToHeadRatio);
-   glRotatef(90, 1, 0, 0);
-   gluCylinder(neckQuad, neckRadius, neckRadius, neckHeight, 10, 10);
-   glPopMatrix();
 }
 
 void drawBody(){  
